af6d41b8cfb4fc66892715db7c82fe45466b0181
pradeepa1997 <pradeepasandharuwan@gmail.com>
2020-06-14 19:07:40 +0530

    counting session done

--- js/home.js
+++ js/home.js
@@ -1,8 +1,68 @@
 window.onscroll = function() {scrollFunction()};
+document.getElementById("header").style.display="none";
 var dropdown=0;
+var temp=100+(screen.height*2);
+var flag=false;
+setTimeout(()=>{
+    document.getElementById("loader").style.display="none";
+    document.getElementById("header").style.display="block";
+},3000);
 
-function scrollFunction() {
-  
+function scrollFunction(){
+    if(document.body.scrollTop > temp || document.documentElement.scrollTop > temp){
+        if(!flag){
+            flag=true;
+            document.getElementById("about-us-line1").style.height="300px";
+            document.getElementById("about-us-circle1").style.marginTop="300px";
+
+            document.getElementById("about-us-line2").style.height="500px";
+            document.getElementById("about-us-circle2").style.marginTop="500px";
+
+            document.getElementById("about-us-line3").style.height="400px";
+            document.getElementById("about-us-circle3").style.marginTop="400px";
+
+            document.getElementById("about-us-line4").style.height="150px";
+            document.getElementById("about-us-circle4").style.marginTop="150px";
+
+
+            var i1=0,i2=0,i3=0,i4=0;
+
+            var interval1=setInterval(()=>{
+                i1++;
+                document.getElementById("about-us-1").innerHTML=i1;
+                if(i1==970){
+                    clearInterval(interval1);
+                    document.getElementById("about-us-1").innerHTML="970";
+                }
+            },2)
+            
+            var interval2=setInterval(()=>{
+                i2+=10;
+                document.getElementById("about-us-2").innerHTML=i2;
+                if(i2==5150){
+                    clearInterval(interval2);
+                    document.getElementById("about-us-2").innerHTML="5,150";
+                }        
+            },7);
+            var interval3=setInterval(()=>{
+                i3+=10;
+                document.getElementById("about-us-3").innerHTML=i3;
+                if(i3==4900){
+                    clearInterval(interval3);
+                    document.getElementById("about-us-3").innerHTML="4,900";
+                }        
+            },7);
+            document.getElementById("about-us-4").innerHTML="0";
+            var interval4=setInterval(()=>{
+                i4++;
+                document.getElementById("about-us-4").innerHTML=i4;
+                if(i4==5){
+                    clearInterval(interval4);
+                    document.getElementById("about-us-4").innerHTML="5 +";
+                }        
+            },100);
+        }
+    }
     if(document.body.scrollTop > 100 || document.documentElement.scrollTop > 100){
         document.getElementById("navbar").style.position="fixed";
         document.getElementById("navbar").style.top="0px";
